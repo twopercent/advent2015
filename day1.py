7be6322c8f96bf2681cbe825f4925dcf709e871b
Chris Moser <chrismmoser@gmail.com>
2015-12-08 12:50:24 -0600

    finished day2

--- day1.py
+++ day1.py
@@ -1,19 +1,25 @@
 #!/bin/env python3
 
 floor = 0
+position = 1
 
 f = open('day1_data.txt', mode='r')
 bracket = f.read(1)
 
 while bracket != '':
-    print(bracket)
+    #print(bracket)
     if bracket == '(':
-        print('up')
+    #   print('up')
         floor += 1
-    if bracket == ')':
-        print('down')
+    elif bracket == ')':
+    #    print('down')
         floor -= 1
+
+    if floor == -1:
+        print('Basement at position ' + str(position))
+
     bracket = f.read(1)
+    position += 1
 
 print(floor)
 
--- day2.py
+++ day2.py
@@ -2,12 +2,43 @@
 
 f = open('day2_data.txt', mode='r')
 
-side1 = 0
-side2 = 0
-side3 = 0
+paper_order = 0
+ribbon_order = 0
 
-dimensions = print(f.readline())
+dimensions = f.readline()[:-1]
+
+while dimensions != '':
+    dimension_list = dimensions.split('x')
+    
+    l = int(dimension_list[0])
+    w = int(dimension_list[1])
+    h = int(dimension_list[2])
+
+    base_paper = 2*l*w + 2*w*h + 2*h*l
+    # The area of the smallest side (the product of the smallest 2 dimensions)
+    # is equal to the product of all dimensions divided by the largest side
+    slack_paper = int(l*w*h / max(l,w,h))
+
+    paper_order += base_paper + slack_paper    
+
+    #It's bow time:
+    int_list = [l,w,h]
+    
+    small = min(int_list)
+    int_list.remove(small)
+
+    medium = min(int_list)
+    
+    large = max(int_list)
+
+    base_ribbon = small + small + medium + medium
+    bow_ribbon = small * medium * large
+
+    ribbon_order += base_ribbon + bow_ribbon
+
+    dimensions = f.readline()[:-1]
 
 
 f.close()
-
+print('Total paper = ' + str(paper_order))
+print('Total ribbon = ' + str(ribbon_order))
