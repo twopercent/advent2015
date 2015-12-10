42cc696d82bbcbc08ebd684af93f092615dbd83d
Chris Moser <chrismmoser@gmail.com>
2015-12-10 11:15:04 -0600

    first half day 5

---
+++ day5.py
@@ -0,0 +1,85 @@
+#! /bin/env python3
+
+f = open('day5_data.txt', mode='r')
+
+nice_words = 0
+word = f.readline()[:-1]
+
+""" This function verifies 3 unique vowels, we need 3 total vowels
+def three_vowels(test_word):
+    vowels = 0
+    if 'a' in test_word:
+        vowels += 1
+    if 'e' in test_word:
+        vowels += 1
+    if 'i' in test_word:
+        vowels += 1
+    if 'o' in test_word:
+        vowels += 1
+    if 'u' in test_word:
+        vowels += 1
+
+    if vowels >= 3:
+        return True
+    else:
+        return False
+"""
+
+def three_vowels(test_word):
+    vowels = 0
+    vowels += test_word.count('a')
+    vowels += test_word.count('e')
+    vowels += test_word.count('i')
+    vowels += test_word.count('o')
+    vowels += test_word.count('u')
+
+    if vowels >= 3:
+        return True
+    else:
+        return False
+
+def appears_twice(test_word):
+    result = False
+    #Set last letter to the first letter of the string
+    last_letter = test_word[:1]
+    # Loop over every letter except the first
+    for this_letter in test_word[1:]:
+        if last_letter == this_letter:
+            result = True
+            break
+        else:
+            last_letter = this_letter
+    return result
+
+def restricted_strings(test_word):
+    result = False
+    restricted_list = ['ab','cd','pq','xy']
+
+    for restricted in restricted_list:
+        if restricted in test_word:
+            result = True
+            break
+    return result
+
+def is_nice(test_word):
+    return three_vowels(test_word) and appears_twice(test_word) and not restricted_strings(test_word) 
+
+# New rules
+
+def two_letters_twice(test_word):
+    result = False
+    
+
+
+
+
+while word != '':
+    if is_nice(word):
+        nice_words += 1
+    print(word + ' Is nice: ' + str(is_nice(word)))
+    #print('Three vowels: ' + str(three_vowels(word)))
+    #print('Consecutive letters: ' + str(appears_twice(word)))
+    #print('Contains ab, cd, pq, xy: ' + str(restricted_strings(word)))
+    word = f.readline()[:-1]
+
+print('Nice words: ' + str(nice_words))
