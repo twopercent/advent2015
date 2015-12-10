35683baa4942f03c17229563005e7681987fc2c2
Chris Moser <chrismmoser@gmail.com>
2015-12-10 13:57:44 -0600

    finished day5

--- day5.py
+++ day5.py
@@ -2,28 +2,33 @@
 
 f = open('day5_data.txt', mode='r')
 
-nice_words = 0
+nice_words_old = 0
+nice_words_new = 0
 word = f.readline()[:-1]
 
-""" This function verifies 3 unique vowels, we need 3 total vowels
-def three_vowels(test_word):
-    vowels = 0
-    if 'a' in test_word:
-        vowels += 1
-    if 'e' in test_word:
-        vowels += 1
-    if 'i' in test_word:
-        vowels += 1
-    if 'o' in test_word:
-        vowels += 1
-    if 'u' in test_word:
-        vowels += 1
 
-    if vowels >= 3:
-        return True
-    else:
-        return False
-"""
+# This function verifies 3 unique vowels, we need 3 total vowels
+#def three_vowels(test_word):
+#    vowels = 0
+#    if 'a' in test_word:
+#        vowels += 1
+#    if 'e' in test_word:
+#        vowels += 1
+#    if 'i' in test_word:
+#        vowels += 1
+#    if 'o' in test_word:
+#        vowels += 1
+#    if 'u' in test_word:
+#        vowels += 1
+#
+#    if vowels >= 3:
+#        return True
+#    else:
+#        return False
+
+#############
+# Old Rules #
+#############
 
 def three_vowels(test_word):
     vowels = 0
@@ -61,25 +66,45 @@ def restricted_strings(test_word):
             break
     return result
 
-def is_nice(test_word):
+def is_nice_old(test_word):
     return three_vowels(test_word) and appears_twice(test_word) and not restricted_strings(test_word) 
 
-# New rules
+#############
+# New rules #
+#############
 
 def two_letters_twice(test_word):
     result = False
-    
+    for index in range(len(test_word) - 1):
+        test_letters = test_word[index:index + 2]
+        #I thought I would need to add a case to invalidate 'aaa' but python returns 'aaa'.count('aa') as 1
+        if test_word.count(test_letters) >= 2:
+            result = True
+            break
+    return result
 
+def one_letter_repeat(test_word):
+    result = False
+    for index in range(len(test_word) - 2):
+        test_letter = test_word[index]
+        if test_letter == test_word[index + 2]:
+            result = True
+            break
+    return result
 
-
+def is_nice_new(test_word):
+    return two_letters_twice(test_word) and one_letter_repeat(test_word)
 
 while word != '':
-    if is_nice(word):
-        nice_words += 1
-    print(word + ' Is nice: ' + str(is_nice(word)))
+    if is_nice_old(word):
+        nice_words_old += 1
+    if is_nice_new(word):
+        nice_words_new += 1
+    #print(word + ' Is nice: ' + str(is_nice(word)))
     #print('Three vowels: ' + str(three_vowels(word)))
     #print('Consecutive letters: ' + str(appears_twice(word)))
     #print('Contains ab, cd, pq, xy: ' + str(restricted_strings(word)))
     word = f.readline()[:-1]
 
-print('Nice words: ' + str(nice_words))
+print('Nice words - old rules: ' + str(nice_words_old))
+print('Nice words - new rules: ' + str(nice_words_new))
